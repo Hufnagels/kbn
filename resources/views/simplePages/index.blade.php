bff1712aa809b795e3c722d194f17ed3504e8a04
Hufnagels <pp@varsoft.hu>
2017-09-14 22:46:31 +0200

    Index page old Image and some styling 4

--- resources/views/simplePages/index.blade.php
+++ resources/views/simplePages/index.blade.php
@@ -55,13 +55,13 @@
       <div class="hero has-text-right">
         <div class="hero-body m-t-20">
           <div class="container">
-            <h1 class="title is-size-3 is-spaced is-uppercase wow bounceInDown" data-wow-duration="2s" data-wow-delay=".1s" style="visibility:hidden;">
+            <h1 class="title is-size-3 is-spaced has-text-kv is-uppercase wow bounceInDown" data-wow-duration="2s" data-wow-delay=".1s" style="visibility:hidden;">
               {{ __('indexPage.slogen1') }}
             </h1>
-            <h2 class="title is-size-3 is-spaced has-text-warning is-uppercase m-t-20 wow bounceInDown" data-wow-duration="5s" data-wow-delay=".1s" style="visibility:hidden;">
+            <h2 class="title is-size-3 is-spaced has-text-kv is-uppercase m-t-0 wow bounceInDown" data-wow-duration="5s" data-wow-delay=".1s" style="visibility:hidden;">
               {{ __('indexPage.slogen2') }}
             </h2>
-            <h2 class="title is-size-3 is-spaced has-text-primary is-uppercase m-t-20 wow bounceInDown" data-wow-duration="8s" data-wow-delay=".1s" style="visibility:hidden;">
+            <h2 class="title is-size-3 is-spaced has-text-kv is-uppercase m-t-0 wow bounceInDown" data-wow-duration="8s" data-wow-delay=".1s" style="visibility:hidden;">
               {{ __('indexPage.slogen3') }}
             </h2>
             <p class="is-size-4 is-spaced has-text-white-bis has-text-weight-semibold m-t-50 wow bounceInDown" data-wow-duration="12s" data-wow-delay=".1s" style="visibility:hidden;">
